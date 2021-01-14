9973fdaa8b3115de54fd373aff142ed65e10ed51
Dy El Alem <dy.el-alem@grenoble-inp.org>
2021-01-14 17:05:41 +0100

    Update ListDeclField.java

--- src/main/java/fr/ensimag/deca/tree/ListDeclField.java
+++ src/main/java/fr/ensimag/deca/tree/ListDeclField.java
@@ -1,21 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fr.ensimag.deca.tree;
 
+import fr.ensimag.deca.DecacCompiler;
+import fr.ensimag.deca.context.ClassDefinition;
+import fr.ensimag.deca.context.ContextualError;
+import fr.ensimag.deca.context.EnvironmentExp;
 import fr.ensimag.deca.tools.IndentPrintStream;
 
 /**
- *
- * @author ensimag
+ * List of class fields declarations .
+ * 
+ * @author gl16
+ * @date 01/01/2021
  */
-public class ListDeclField  extends TreeList<AbstractDeclField>{
+public class ListDeclField extends TreeList<AbstractDeclField> {
 
     @Override
     public void decompile(IndentPrintStream s) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    /**
+     * Implements non-terminal "list_decl_field" of [SyntaxeContextuelle] in pass 2
+     * @param compiler contains the "env_types" attribute
+     * @param localEnv 
+     *   its "parentEnvironment" corresponds to "env_exp_sup" attribute
+     *   in precondition, its "current" dictionary corresponds to 
+     *      the "env_exp" attribute
+     *   in postcondition, its "current" dictionary corresponds to 
+     *      the "env_exp_r" attribute
+     * @param currentClass 
+     *          corresponds to "class" attribute (null in the main bloc).
+     */    
+    void verifyListDeclField(DecacCompiler compiler, EnvironmentExp localEnv,
+            ClassDefinition currentClass) throws ContextualError {
+    	for (AbstractDeclField declField: this.getList()) {
+    		declField.verifyDeclField(compiler, localEnv, currentClass);
+    	}
     }
     
 }
